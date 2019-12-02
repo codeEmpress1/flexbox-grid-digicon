a58f6ce9684c39903233e31332e7618b4049ad2e
CoderHal <halof.halima@gmail.com>
2019-12-02 09:18:57 +0100

    Task completed

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var link = document.querySelector('#links');
+function show() {
+    if(link.style.display == 'block') {
+        link.style.display = 'none';
+    }
+    else link.style.display = 'block';
+        
+}
